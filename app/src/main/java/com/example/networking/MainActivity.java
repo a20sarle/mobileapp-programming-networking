89e7f36fa55ea359485d7afae3c0cb97f6f69025
a20sarle <a20sarle.student@his.se>
2021-04-27 12:09:04 +0200

    added method to read the mountains.json file

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,14 +3,30 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ListView;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+
+    @SuppressWarnings("SameParameterValue")
+    private String readFile(String fileName) {
+        try {
+            //noinspection CharsetObjectCanBeUsed
+            return new Scanner(getApplicationContext().getAssets().open(fileName), Charset.forName("UTF-8").name()).useDelimiter("\\A").next();
+        } catch (IOException e) {
+            Log.e("TAG", "Could not read file: " + fileName);
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
