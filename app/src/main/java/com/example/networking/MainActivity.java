7b2702f1d5f1856a1db095799403300455874074
a20sarle <a20sarle.student@his.se>
2021-04-27 11:53:34 +0200

    Initiated WebView in MainActivity.java

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,13 +3,23 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webbSettings=webView.getSettings();
+        webbSettings.setJavaScriptEnabled(true);
     }
 }
