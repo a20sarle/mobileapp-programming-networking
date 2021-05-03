4ac4beacc7caf097fe381855d450ffea50caa1d1
a20sarle <a20sarle.student@his.se>
2021-05-03 11:38:10 +0200

    removed unused members

--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -1,19 +1,9 @@
 package com.example.networking;
 
-public class Mountain {
-    private String ID;
+public class Mountain <string> {
     private String name;
-    private String type;
-    private String company;
     private String location;
-    private String category;
     private int height;
-    private String description;
-
-    private int size;
-    private int cost;
-
-    private Auxdata auxdata;
 
     public Mountain(String name, int height, String location) {
         this.name=name;
@@ -26,14 +16,6 @@ public class Mountain {
     }
     public int getHeight(String height) { return this.height; }
     public String getLocation(String location) { return this.location; }
-
-    public String getLocation() {
-        return location;
-    }
-    public int getSize(){return size; }
-    public Auxdata getAuxdata() {
-        return auxdata;
-    }
 
     @Override
     public String toString(){return name;}
